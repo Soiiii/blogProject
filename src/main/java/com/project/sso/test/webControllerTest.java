e567a6237dc33d66fbb9bd8c03969bcc3e3a2bff
Soiiii <cojomkr33@gmail.com>
2023-05-17 21:49:18 +0900

    commit

---
+++ src/main/java/com/project/sso/test/webControllerTest.java
@@ -0,0 +1,13 @@
+package com.project.sso.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class webControllerTest {
+        @GetMapping("/test/hello")
+        public String hello(){
+            return "<h1>hello spring boot</h1>" ;
+
+    }
+}
