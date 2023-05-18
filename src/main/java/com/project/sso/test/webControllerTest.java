dc90aff6085b31da7a772d084372e6644b92f8d2
Soiiii <cojomkr33@gmail.com>
2023-05-18 23:59:21 +0900

    blog project setup

--- src/main/java/com/project/sso/test/webControllerTest.java
+++ src/main/java/com/project/sso/test/webControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class webControllerTest {
         @GetMapping("/test/hello")
         public String hello(){
+            System.out.println("test");
             return "<h1>hello spring boot</h1>" ;
 
     }
