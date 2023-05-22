0ed22b70cf617b938054d6b0fb7b1d3fdcf8c145
Soiiii <cojomkr33@gmail.com>
2023-05-23 06:44:31 +0900

    blog project setup

--- src/main/java/com/project/sso/model/Board.java
+++ src/main/java/com/project/sso/model/Board.java
@@ -1,4 +1,33 @@
 package com.project.sso.model;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
 public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length=100)
+    private String title;
+
+    @Lob
+    private String content;
+
+    @ColumnDefault("0")
+    private int count;
+
+    //DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다
+    @ManyToOne
+    @JoinColumn(name="userId")
+    private User user;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+
 }
