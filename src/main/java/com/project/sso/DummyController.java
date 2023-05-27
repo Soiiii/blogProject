1928963695e04ca83a14159d6a15d7eaa02adc2f
Soiiii <cojomkr33@gmail.com>
2023-05-27 23:49:01 +0900

    blog project

--- src/main/java/com/project/sso/DummyController.java
+++ src/main/java/com/project/sso/DummyController.java
@@ -4,10 +4,16 @@ import com.project.sso.model.RoleType;
 import com.project.sso.model.User;
 import com.project.sso.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DummyController {
@@ -21,6 +27,23 @@ public class DummyController {
             return new IllegalArgumentException("해당 사용자는 없습니다.");
         });
         return user;
+    }
+
+    @GetMapping("/dummy/users")
+    public List<User> list(){
+        System.out.println("findall All the list");
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/dummy/user")
+    public List<User> pageList(@PageableDefault(size=4, sort="id", direction= Sort.Direction.DESC) Pageable pageable){
+        Page<User> pagingUser = userRepository.findAll(pageable);
+        if(pagingUser.isLast()){
+        }
+        List<User> users = pagingUser.getContent();
+        return users;
+
+
     }
 
     @PostMapping("/dummy/join")
